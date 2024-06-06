824c9994d89bb826c36a9896194f3366592cd7f7
Erik Howard <djribs@gmail.com>
2024-06-06 13:35:35 -0500

    initial push

---
+++ business-card-generator.js
@@ -0,0 +1,194 @@
+/**
+
+This file will run a Photoshop script that shows a dialog. The dialog will allow the input of 6 fields:
+
+Full Name
+Title
+Area Code
+Phone
+Ext
+Email
+
+The only requirement is that the photoshop document being targeted, 
+(which must be open in Photoshop before running this script),
+contains the following text layers:
+
+tf_name
+tf_title
+tf_phone
+tf_email
+
+The test layers can be nested in other layers or groups. The code will find them.
+
+The created PNG file will be saved in ~/Downloads when the script is executed,
+named following the format of firstname-lastname-card.png
+
+**/
+
+#target photoshop
+
+var docRef = app.activeDocument;
+var emailBase = '@site.com'; // change to your domain
+var username, title, phone, ext, email = '';
+var u;
+
+var dlg = new Window ('dialog', 'JDV Card Creator', u);
+dlg.location = [100,100];
+dlg.margins = 15;
+dlg.orientation = 'stack';
+dlg.alignChildren = ['fill', 'fill'];
+
+var mainPanel = dlg.add('panel', u, 'Enter Card Values');
+mainPanel.orientation = 'column';
+mainPanel.alignChildren = 'right';
+mainPanel.margins = [15, 20, 15, 15];
+
+var confirmPanel = dlg.add('panel', u, 'Confirm');
+confirmPanel.orientation = 'column';
+confirmPanel.alignChildren = ['fill', -1];
+confirmPanel.margins = [15, 20, 15, 15];
+var confirmName = confirmPanel.add('statictext', u, '-');
+var confirmTitle = confirmPanel.add('statictext', u, '-');
+var confirmPhone = confirmPanel.add('statictext', u, '-');
+var confirmEmail = confirmPanel.add('statictext', u, '-');
+
+var btnContainer = confirmPanel.add( 'panel', u, u, {borderStyle: 'none'});
+btnContainer.orientation = 'row';
+btnContainer.alignment = 'center';
+btnContainer.alignChildren = ['fill', 'fill'];
+var btnBuild = btnContainer.add('button', u, 'Build');
+btnBuild.onClick = populateFields;
+var btnBack = btnContainer.add('button', u, 'Back');
+btnBack.onClick = goBack;
+
+var nameContainer = mainPanel.add( 'group' );
+nameContainer.orientation = 'row';
+nameContainer.add('statictext', u, 'Full Name:');
+var nameField = nameContainer.add('edittext', u);
+nameField.characters = 30;
+
+var titleContainer = mainPanel.add( 'group' );
+titleContainer.orientation = 'row';
+titleContainer.add('statictext', u, 'Title:');
+var titleField = titleContainer.add('edittext', u);
+titleField.characters = 30;
+
+var phoneContainer = mainPanel.add( 'group' );
+phoneContainer.add('statictext', u, 'Area Code:');
+var areaCodeField = phoneContainer.add('edittext', u);
+areaCodeField.justify = 'center';
+areaCodeField.characters = 4;
+phoneContainer.add('statictext', u, '      Phone:');
+var phoneField = phoneContainer.add('edittext', u);
+phoneField.justify = 'center';
+phoneField.characters = 8;
+phoneContainer.add('statictext', u, ' Ext:');
+var extField = phoneContainer.add('edittext', u);
+extField.justify = 'center';
+extField.characters = 4;
+
+var emailContainer = mainPanel.add( 'group' );
+emailContainer.add('statictext', u, 'Email:');
+var emailField = emailContainer.add('edittext', u);
+emailField.characters = 21;
+emailField.justify = 'right';
+emailContainer.add('statictext', u, '@dv-llc.com');
+
+var btnPreviewContainer = mainPanel.add( 'panel', u, u, {borderStyle: 'none'});
+btnPreviewContainer.alignment = 'center';
+btnPreviewContainer.alignChildren = ['fill', 'fill'];
+var btnPreview = btnPreviewContainer.add('button', u, 'Preview');
+btnPreview.onClick = showPreview;
+
+function showPreview() {
+  username = nameField.text;
+  title = titleField.text;
+  ext = extField.text == '' ? '' : ' x' + extField.text;
+  phone = '(' + areaCodeField.text + ') ' + 
+                phoneField.text.substr(0, 3) + '-' + 
+                phoneField.text.substr(3) + 
+                ext;
+
+  email = emailField.text + emailBase;
+
+  confirmName.text = ' Name:  ' + username;
+  confirmTitle.text = '   Title:  ' + title;
+  confirmPhone.text = 'Phone:  ' + phone;
+  confirmEmail.text = '  Email:  ' + email;
+
+  mainPanel.visible = false;
+}
+
+function goBack() {
+  mainPanel.visible = true;
+}
+
+function populateFields() {
+  changeTextLayerContent(docRef, 'tf_name', username);
+  changeTextLayerContent(docRef, 'tf_title', title);
+  changeTextLayerContent(docRef, 'tf_phone', phone);
+  changeTextLayerContent(docRef, 'tf_email', email);
+
+  finalize();
+}
+
+function finalize() {
+  if(confirm('Click OK to save to /Downloads')) {
+    dlg.close();
+    saveFile();
+  } else {
+    mainPanel.visible = true;
+  }
+  
+}
+
+function saveFile() {
+  var pngFile = new File('~/Downloads/' + username.split(' ').join('-').toLowerCase() + '-card.png');
+  var pngSaveOptions = new PNGSaveOptions();
+
+  pngSaveOptions.embedColorProfile = true;
+  pngSaveOptions.formatOptions = FormatOptions.STANDARDBASELINE;
+  pngSaveOptions.matte = MatteType.NONE;
+  pngSaveOptions.quality = 1;
+
+  docRef.saveAs(pngFile, pngSaveOptions, true, Extension.LOWERCASE);
+}
+
+function changeTextLayerContent(doc, layerName, newTextString) {
+  for (var i = 0, max = doc.layers.length; i < max; i++) {
+    var layerRef = doc.layers[i];
+    if (layerRef.typename === 'ArtLayer') {
+      if (layerRef.name === layerName && layerRef.kind === LayerKind.TEXT) {
+        layerRef.textItem.contents = newTextString;
+      }
+    } else {
+      changeTextLayerContent(layerRef, layerName, newTextString);
+    }
+  }
+}
+
+dlg.show ();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
